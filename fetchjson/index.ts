f1e48f0c8787a328a7b5a897d3a36adab285f2ab
abaez8031 <abaez8031@gmail.com>
2024-07-19 13:27:51 -0400

    interface

--- fetchjson/index.ts
+++ fetchjson/index.ts
@@ -1,6 +1,22 @@
 import axios from "axios";
 
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
 const url = "https://jsonplaceholder.typicode.com/todos/1"
 axios.get(url).then(response => {
-  console.log(response.data)
+  const todo = response.data as Todo;
+  const {id, title, completed} = todo;
+  logTodo(id,title,completed)
 })
+
+const logTodo = (id: number,title: string,completed: boolean) => {
+  console.log(`
+    The todo with ID: ${id}
+    Has a title of: ${title}
+    Is it finished? ${completed}
+    `)
+}
