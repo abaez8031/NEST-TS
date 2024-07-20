17b8684e537b20ffdde48f92f46450406833da11
abaez8031 <abaez8031@gmail.com>
2024-07-20 01:00:50 -0400

    types

---
+++ annotations/variables.ts
@@ -0,0 +1,8 @@
+let apples: number = 5;
+let speed: string = "fast";
+let hasName: boolean = true;
+let nothingMuch: null = null;
+let now: Date = new Date();
+let colors: string[] = ["red", "blue", "green"]
+let myNums: number[] = [1,2,3]
+let truths: boolean[] = [true, true, false]
