951e5ec9ce9e8089c4c080c2c4b133ec92f39d45
abaez8031 <abaez8031@gmail.com>
2024-07-22 11:38:56 -0400

    destructuringargs

--- features/annotations/functions.ts
+++ features/annotations/functions.ts
@@ -1,3 +1,36 @@
 const add = (a: number, b: number): number => {
-  return a + b
+  return a + b;
+};
+// There is no type inference for arguments
+// We always have to use type annotations
+// Type inference works for output but we should explicitly use annotations, in case we forget return statement or return incorrect type
+
+function divide(a: number, b: number): number {
+  return a / b;
+}
+
+const multiply = function (a: number, b: number): number {
+  return a * b;
+};
+
+const logger = (message: string): void => {
+  console.log(message);
+  // void can return null or undefined but is used when no return value
+  // return null;
+  // return undefined;
+}
+
+const throwError = (message: string): never => {
+  throw new Error(message)
+}
+// never is only used if you never expect a function to return anything ever
+
+const forecast = {
+  date: new Date(),
+  weather: "sunny"
+}
+
+const logWeather = ({ date, weather }: {date: Date; weather: string}): void => {
+  console.log(date)
+  console.log(weather)
 }
