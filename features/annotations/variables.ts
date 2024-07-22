a48732d0b0c6eafbfc9bc1a57ee7adc49a585261
abaez8031 <abaez8031@gmail.com>
2024-07-21 21:32:15 -0400

    annotations

--- features/annotations/variables.ts
+++ features/annotations/variables.ts
@@ -1,8 +1,12 @@
+// Primitive data types
 let apples: number = 5;
 let speed: string = "fast";
 let hasName: boolean = true;
 let nothingMuch: null = null;
+let nothing: undefined = undefined
 let now: Date = new Date();
+// Arrays
 let colors: string[] = ["red", "blue", "green"]
 let myNums: number[] = [1,2,3]
 let truths: boolean[] = [true, true, false]
+//Classes
