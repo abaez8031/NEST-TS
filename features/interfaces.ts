083012e43be5f84f341bd31055ce75bcb21c80d5
abaez8031 <abaez8031@gmail.com>
2024-07-22 14:06:57 -0400

    interface

---
+++ features/interfaces.ts
@@ -0,0 +1,17 @@
+interface Vehicle {
+  name: string,
+  year: number,
+  broken: ConstrainBoolean
+}
+
+const oldCivic = {
+  name: "Civic",
+  year: 2000,
+  broken: true
+}
+
+const printVehicle = (vehicle: Vehicle): void => {
+  console.log(`Name: ${vehicle.name}`);
+  console.log(`Year: ${vehicle.year}`);
+  console.log(`Broken?: ${vehicle.broken}`);
+}
