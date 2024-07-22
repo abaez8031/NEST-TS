09d7dcff4d3ba262f35270f75572e2785dfd1b58
abaez8031 <abaez8031@gmail.com>
2024-07-22 15:23:35 -0400

    classes

--- features/classes.ts
+++ features/classes.ts
@@ -1,19 +1,28 @@
-class Vehicle {
-  drive(): void {
-    console.log("chugga chugga")
-  }
+// public methods - can be called anywhere, anytime
+// private methods - can only be called by other methods inside of the class
+// protected methods - can be called by methods in this class or child class
 
-  honk(): void {
+class Vehicle {
+  constructor(public color: string) {}
+  protected honk(): void {
     console.log("beep")
   }
 }
 
+const vehicle = new Vehicle("orange");
+
 class Car extends Vehicle {
-  drive(): void {
+  constructor(public wheels: number, color: string) {
+    super(color);
+  }
+  private drive(): void {
     console.log("vroom vroom")
+  }
+  startDrivingProcess(): void {
+    this.drive();
+    this.honk()
   }
 }
 
-const car = new Car();
-car.drive();
-car.honk();
+const car = new Car(4, "red");
+car.startDrivingProcess()
