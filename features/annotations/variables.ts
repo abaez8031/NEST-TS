800e94631ecffa147f12ef4c2b50302da018c878
abaez8031 <abaez8031@gmail.com>
2024-07-21 21:36:26 -0400

    classes&objects

--- features/annotations/variables.ts
+++ features/annotations/variables.ts
@@ -5,8 +5,21 @@ let hasName: boolean = true;
 let nothingMuch: null = null;
 let nothing: undefined = undefined
 let now: Date = new Date();
+
 // Arrays
 let colors: string[] = ["red", "blue", "green"]
 let myNums: number[] = [1,2,3]
 let truths: boolean[] = [true, true, false]
+
 //Classes
+class Car {
+
+}
+
+let car: Car = new Car()
+
+// Object Literals
+let point: { x: number; y: number; } = {
+  x: 10,
+  y: 20
+}
