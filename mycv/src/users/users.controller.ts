e9592777612f67ad0368e4751b6c896eacb9c40a
abaez8031 <abaez8031@gmail.com>
2024-08-03 17:16:27 -0400

    usersservice

--- mycv/src/users/users.controller.ts
+++ mycv/src/users/users.controller.ts
@@ -7,13 +7,15 @@ import {
   Param,
   Body,
 } from '@nestjs/common';
+import { UsersService } from './users.service';
 import { CreateUserDto } from './dtos/create-user.dto';
 
 @Controller('auth')
 export class UsersController {
+  constructor(private usersService: UsersService) {}
   @Post('/signup')
   createUser(@Body() body: CreateUserDto) {
-    console.log(body);
+    this.usersService.create(body.email, body.password);
   }
 
   @Get('/:id')
