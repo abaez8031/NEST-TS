4478e3485e4cc630c48c053fccd96754806b9431
abaez8031 <abaez8031@gmail.com>
2024-08-03 18:21:43 -0400

    userscontroller

--- mycv/src/users/users.controller.ts
+++ mycv/src/users/users.controller.ts
@@ -6,6 +6,7 @@ import {
   Delete,
   Param,
   Body,
+  Query,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dtos/create-user.dto';
@@ -20,15 +21,19 @@ export class UsersController {
 
   @Get('/:id')
   findUser(@Param('id') id: string) {
-    console.log(id);
+    this.usersService.findOne(parseInt(id));
   }
 
   @Get('')
-  findAllUsers() {}
+  findAllUsers(@Query('email') email: string) {
+    return this.usersService.find(email);
+  }
 
   @Patch('/:id')
   updateUser() {}
 
   @Delete('/:id')
-  deleteUser() {}
+  deleteUser(@Param('id') id: string) {
+    this.usersService.remove(parseInt(id));
+  }
 }
