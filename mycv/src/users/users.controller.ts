bf16a71c84d5ab6cd296051a29582dd41bac32a9
abaez8031 <abaez8031@gmail.com>
2024-08-03 16:22:20 -0400

    createuserdto

--- mycv/src/users/users.controller.ts
+++ mycv/src/users/users.controller.ts
@@ -1,12 +1,25 @@
-import { Controller, Post, Get, Patch, Delete, Param, Body } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Get,
+  Patch,
+  Delete,
+  Param,
+  Body,
+} from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
 
 @Controller('auth')
 export class UsersController {
   @Post('/signup')
-  createUser() {}
+  createUser(@Body() body: CreateUserDto) {
+    console.log(body);
+  }
 
   @Get('/:id')
-  findUser(@Param('id') id: string) {}
+  findUser(@Param('id') id: string) {
+    console.log(id);
+  }
 
   @Get('')
   findAllUsers() {}
