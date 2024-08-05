a01caad1bd0a3d983faa54fa4a596577cf339e83
abaez8031 <abaez8031@gmail.com>
2024-08-04 22:17:44 -0400

    exceptions

--- mycv/src/users/users.controller.ts
+++ mycv/src/users/users.controller.ts
@@ -7,6 +7,7 @@ import {
   Param,
   Body,
   Query,
+  NotFoundException,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dtos/create-user.dto';
@@ -21,8 +22,10 @@ export class UsersController {
   }
 
   @Get('/:id')
-  findUser(@Param('id') id: string) {
-    this.usersService.findOne(parseInt(id));
+  async findUser(@Param('id') id: string) {
+    const user = await this.usersService.findOne(parseInt(id));
+    if (!user) throw new NotFoundException('User not found');
+    return user;
   }
 
   @Get('')
