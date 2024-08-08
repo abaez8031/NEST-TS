a7463893a521fa1e0d46af17d9dcbf431ac407ba
abaez8031 <abaez8031@gmail.com>
2024-08-08 16:25:54 -0400

    auth

--- mycv/src/users/auth.service.ts
+++ mycv/src/users/auth.service.ts
@@ -1,15 +1,28 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { randomBytes, scrypt as _scrypt } from 'crypto';
+import { promisify } from 'util';
+
+const scrypt = promisify(_scrypt);
 
 @Injectable()
 export class AuthService {
   constructor(private usersService: UsersService) {}
 
   async signup(email: string, password: string) {
+    // See if email is already in use
     const users = await this.usersService.find(email);
     if (users.length) {
       throw new BadRequestException('Email is already in use');
     }
+    // Generate a salt
+    const salt = randomBytes(8).toString('hex');
+    // Hash the salt and password together
+    const hash = (await scrypt(password, salt, 32)) as Buffer;
+    // Join the hashed result and the salt
+    const result = salt + '.' + hash.toString('hex');
+
+    // Create and store the new user
     return this.usersService.create(email, password);
   }
   signin() {}
