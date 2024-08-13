124d7e493d169cb630ae1c4671678bd9384663c0
abaez8031 <abaez8031@gmail.com>
2024-08-13 02:22:51 -0400

    controllertesting

--- mycv/src/users/users.controller.spec.ts
+++ mycv/src/users/users.controller.spec.ts
@@ -1,10 +1,40 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
+import { AuthService } from './auth.service';
+import { UsersService } from './users.service';
+import { User } from './user.entity';
+import { NotFoundException } from '@nestjs/common';
 
 describe('UsersController', () => {
   let controller: UsersController;
+  let fakeUsersService: Partial<UsersService>;
+  let fakeAuthService: Partial<AuthService>;
 
   beforeEach(async () => {
+    fakeUsersService = {
+      findOne: (id: number) => {
+        return Promise.resolve({
+          id,
+          email: 'asdf@asdf.com',
+          password: 'asdf',
+        } as User);
+      },
+      find: (email: string) => {
+        return Promise.resolve([
+          {
+            id: 1,
+            email,
+            password: 'asdf',
+          },
+        ]);
+      },
+      // remove: () => {},
+      // update: () => {},
+    };
+    fakeAuthService = {
+      //signup: () => {},
+      //signin: () => {}
+    };
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
     }).compile();
@@ -14,5 +44,18 @@ describe('UsersController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+  it('findAllUsers returns a list of users with the given email', async () => {
+    const users = await controller.findAllUsers('asdf@asdf.com');
+    expect(users.length).toEqual(1);
+    expect(users[0].email).toEqual('asdf@asdf.com');
+  })
+  it('findUser returns a single user with the given id', async () => {
+    const user = await controller.findUser('1');
+    expect(user).toBeDefined();
+  });
+  it('findUser throws an error if user with the given id is not found', async () => {
+    fakeUsersService.findOne = () => null;
+    expect(await controller.findUser('1')).rejects.toThrow(NotFoundException);
   });
 });
