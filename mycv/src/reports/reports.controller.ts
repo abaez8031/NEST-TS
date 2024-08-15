f626927b93f1d7d066e56b99e54ba358b3807766
abaez8031 <abaez8031@gmail.com>
2024-08-15 14:05:00 -0400

    guard

--- mycv/src/reports/reports.controller.ts
+++ mycv/src/reports/reports.controller.ts
@@ -1,11 +1,14 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, UseGuards } from '@nestjs/common';
 import { ReportsService } from './reports.service';
+import { AuthGuard } from 'src/guards/auth.guard';
 
 @Controller('reports')
 export class ReportsController {
   constructor(private reportsService: ReportsService) {}
 
   @Post()
-  async createReport(@Body() body: CreateReportDto) {}
-
+  @UseGuards(AuthGuard)
+  async createReport(@Body() body: CreateReportDto) {
+    return this.reportsService.create(body);
+  }
 }
