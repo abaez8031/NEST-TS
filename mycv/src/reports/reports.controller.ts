2308891e1254977acff7c75ed3cd4e93e156fefd
abaez8031 <abaez8031@gmail.com>
2024-08-17 13:47:03 -0400

    report

--- mycv/src/reports/reports.controller.ts
+++ mycv/src/reports/reports.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Post, Body, UseGuards } from '@nestjs/common';
 import { ReportsService } from './reports.service';
 import { AuthGuard } from 'src/guards/auth.guard';
+import { CreateReportDto } from './dtos/create-report.dto';
 
 @Controller('reports')
 export class ReportsController {
